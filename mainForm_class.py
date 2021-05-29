67951c42ba15d041ba13dc901553cfed389eafd1
Fly-Hamster <malich.s.g@gmail.com>
2021-05-29 18:37:46 +0300

    add label_info, def for open file and mute some elements

--- mainForm_class.py
+++ mainForm_class.py
@@ -1,19 +1,39 @@
 from mainForm import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 import helpForm_class
+import errorForm_class
 class MainForm(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.textEdit_output_field.setReadOnly(True)
         self.progressBar.setProperty("value", None)
-
-        self.action.triggered.connect(self.openDoc)
-        self.action_2.triggered.connect(self.saveResult)
-        self.action_3.triggered.connect(self.open_help)
+        self.progressBar.setEnabled(False)
+        # start help
+        self.label_info_1.setText('<html><head/><body><p align=\"center\"><span style=\" font-size:10pt;\">Загрузите файл *.csv..</span></p></body></html>')
+        self.label_info_2.setText('<html><head/><body><p align=\"center\"><span style=\" font-size:10pt;\">Нажмите кнопку "Начать поиск".</span></p></body></html>')
+        # mute start button and button for save result
+        self.button_play.setEnabled(False)
+        self.saveFile.setEnabled(False)
+        # connect menu's actions with functions
+        self.loadFile.triggered.connect(self.openDoc)
+        self.saveFile.triggered.connect(self.saveResult)
+        self.openHelp.triggered.connect(self.open_help)
 
     def openDoc(self):
-        print('openDoc')
+        self.fileName, val = QtWidgets.QFileDialog.getOpenFileName()
+        # checkin the selected file's type
+        if 'csv' in self.fileName:
+            # сделать вывод количества столбцов и считать ввод номера, только потом открыть кнопку
+            self.button_play.setEnabled(True)
+        else:
+            f_err = open('error_text.txt', 'w', encoding='utf-8')
+            f_err.write(' Ошибка при открытии файла.\n Возможно, выбран другой формат.\n Откройте файл с расширением .csv.')
+            f_err.close()
+            win = errorForm_class.ErrorForm()
+            win.setFixedSize(640, 302)
+            win.show()
+
 
     def saveResult(self):
         print("save")
@@ -24,6 +44,10 @@ class MainForm(QtWidgets.QMainWindow, Ui_MainWindow):
         win.setFixedSize(628, 336)
         win.show()
 
+    def start(self):
+        print('start')
+        # открыть прогресс бар
 
+        # в конце открыть кнопку сохранить
 
 
