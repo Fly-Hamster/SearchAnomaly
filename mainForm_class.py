30735d5ed9ae8328d6243d5810ec54df6c7c766c
Fly-Hamster <malich.s.g@gmail.com>
2021-05-29 22:39:03 +0300

    addchekin columns

--- mainForm_class.py
+++ mainForm_class.py
@@ -1,5 +1,10 @@
+
+# import librarys
 from mainForm import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
+import csv
+
+# import files
 import helpForm_class
 import errorForm_class
 class MainForm(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -15,6 +20,7 @@ class MainForm(QtWidgets.QMainWindow, Ui_MainWindow):
         # mute start button and button for save result
         self.button_play.setEnabled(False)
         self.saveFile.setEnabled(False)
+        self.lineEdit_number_column.setEnabled(False)
         # connect menu's actions with functions
         self.loadFile.triggered.connect(self.openDoc)
         self.saveFile.triggered.connect(self.saveResult)
@@ -25,7 +31,29 @@ class MainForm(QtWidgets.QMainWindow, Ui_MainWindow):
         # checkin the selected file's type
         if 'csv' in self.fileName:
             # сделать вывод количества столбцов и считать ввод номера, только потом открыть кнопку
-            self.button_play.setEnabled(True)
+            fileForWork = open(self.fileName, 'r')
+            text = csv.reader(fileForWork)
+            all_columns = []
+            for i in text:
+                all_columns.append(i)
+            # count the number of columns (number delimeters + 1)
+            number_columns = all_columns[0][0].count(';')+1
+
+            if number_columns<2:
+                f_err = open('error_text.txt', 'w', encoding='utf-8')
+                f_err.write(' Файл содержит меньше 2 столбцов. \n Должно быть минимум 2:\n -временной показатель;\n -ряд.')
+                f_err.close()
+                win = errorForm_class.ErrorForm()
+                win.setFixedSize(640, 302)
+                win.show()
+            elif number_columns>=2:
+                text = 'Количество столбцов _'+str(number_columns)+'_'
+                # output information about number columns and user can chooese
+                self.label_number_column.setText(text)
+                self.label_number_column.setFont(QtGui.QFont("Times", 10))
+                self.lineEdit_number_column.setEnabled(True)
+                self.lineEdit_number_column.setInputMask('999')
+                self.button_play.setEnabled(True)
         else:
             f_err = open('error_text.txt', 'w', encoding='utf-8')
             f_err.write(' Ошибка при открытии файла.\n Возможно, выбран другой формат.\n Откройте файл с расширением .csv.')
@@ -46,6 +74,7 @@ class MainForm(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def start(self):
         print('start')
+
         # открыть прогресс бар
 
         # в конце открыть кнопку сохранить
